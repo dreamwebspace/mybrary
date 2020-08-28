87a1f5484f049f0ac3e494de561314200192fab8
dreamwebspace <dev@dreamweb.space>
2020-08-28 09:56:56 +0100

    layout

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use("expressLayouts");
+app.use(static("public"));
